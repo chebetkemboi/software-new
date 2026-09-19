72ec9e6b3a3ddb7e4cb6fd39042a5e4f2337e5e7
chebet kemboi <elizabethchep3@gmail.com>
2026-09-19 19:17:44 +0300

    Project 3 assignment

--- Project/game.py
+++ Project/game.py
@@ -10,25 +10,60 @@ else:
     print("Hello," + Name + "!")
     print("Welcome to Unravel")
 
-while True:
-    print()
-    print("=== MAIN MENU ===")
-    print("1. Start Game")
-    print("2. Check inventory")
-    print("3. View proggrees")
-    print("4. Lopeta")
+    inventory = []
 
-    command = input("Select your choice (1-4): ")
+    def start_game():
+        print("Starting the game...")
 
-    if command == "1":
-       print("Starting the game...") 
-    elif command == "2":
-       print("Checking inventory...")  
-    elif command == "3":
-       print("Viewing progress...") 
-    elif command == "4":
-       print("Exiting the game. Goodbye!")
-       break
-    else:
-       print("Invalid choice. Please select a valid option (1-4).")
+    def find_item():
+        item = input("What item did you find? ")
+        inventory.append(item)
+        print(item, "has been added to your inventory.")
+
+    def check_inventory():
+        print("Your inventory:")
+
+        for item in inventory:
+            print(item)
+
+    def view_progress():
+        print("Viewing progress...")
+
+
+    while True:
+        print()
+        print("=== MAIN MENU ===")
+        print("1. Start Game")
+        print("2. Find an item")
+        print("3. Check inventory")
+        print("4. View progress")
+        print("5. Lopeta")
+
+        command = input("Select your choice (1-5): ")
+
+        if command == "1":
+            start_game()
+
+        elif command == "2":
+            find_item()
+
+        elif command == "3":
+            check_inventory()
+
+        elif command == "4":
+            view_progress()
+
+        elif command == "5":
+            print("Exiting the game. Goodbye!")
+            break
+
+        else:
+            print("Invalid choice. Please select a valid option (1-5).")
+
+
+
+
+
+   
+   
        
